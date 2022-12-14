9f8f9a0148fcaf319bca2a172f326967ccd4950d
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2022-12-14 19:30:45 +0530

    Calculate emp Wage

--- EmployeeChecks.cs
+++ EmployeeChecks.cs
@@ -12,6 +12,10 @@ namespace Employeewageproblem
 
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(2);
@@ -19,11 +23,17 @@ namespace Employeewageproblem
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
-
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+            {
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+
+                Console.WriteLine("EmpWage : " + empWage);
             }
         }
     }
