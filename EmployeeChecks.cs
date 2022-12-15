70da7e43c03ad6029766c8b40c8658e3e18f8859
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2022-12-15 23:25:11 +0530

    Employee Part Time

--- EmployeeChecks.cs
+++ EmployeeChecks.cs
@@ -8,33 +8,38 @@ namespace Employeewageproblem
 {
     public class EmployeeCheck
     {
+       
+
         public static void CheckAttendance()
 
         {
             int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-
+            int IS_PART_TIME = 2;
             int empHrs = 0;
+            int EMP_RATE_PER_HOUR = 20;
             int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+               
+                empHrs = 0;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                
                 empHrs = 0;
             }
-            {
-                empWage = empHrs * EMP_RATE_PER_HOUR;
 
-                Console.WriteLine("EmpWage : " + empWage);
-            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage : " + empWage);
         }
     }
 }
